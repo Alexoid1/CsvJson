20c9a0f554a7313b45991f03674c2afea1dd8157
alexoid1 <alexzambra2610@gmail.com>
2023-06-06 22:59:18 -0500

    gem csvJson

---
+++ lib/csvJson/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CsvJson
+  VERSION = "0.1.0"
+end
---
+++ spec/csvJson_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CsvJson do
+  it "has a version number" do
+    expect(CsvJson::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
